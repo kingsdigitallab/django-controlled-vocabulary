6d320c05047c526513a3ad479dc07ab2ddbf374b
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2020-01-31 17:03:33 +0000

    Improve: Converted evil double quotes to single quotes.

--- controlled_vocabulary/settings.py
+++ controlled_vocabulary/settings.py
@@ -4,24 +4,26 @@ from .defaults import *  # noqa
 
 from django.conf import settings
 
-"""
-Default settings for the controlled_vocabulary app
+'''
+Default dynamic settings for the controlled_vocabulary app
 All settings variables can be overridden in your django project settings.py
-"""
+
+See controlled_vocabulary/defaults.py for static settings.
+'''
 
 # The absolute path to the folder where vocabulary files will be downloaded
 CONTROLLED_VOCABULARY_DATA_ROOT = os.path.join(
-    settings.MEDIA_ROOT, "vocabularies")
+    settings.MEDIA_ROOT, 'vocabularies')
 
 
 def get_var(name):
-    """
+    '''
     Returns the value of a settings variable.
     The full name is CONTROLLED_VOCABULARY_ + name.
     First look into django settings.
     If not found there, use the value defined in this file.
-    """
-    full_name = "CONTROLLED_VOCABULARY_" + name
+    '''
+    full_name = 'CONTROLLED_VOCABULARY_' + name
     ret = globals().get(full_name, None)
     ret = getattr(settings, full_name, ret)
     return ret
